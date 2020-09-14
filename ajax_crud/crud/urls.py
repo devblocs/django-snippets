a2c06c64cc13b6459e2a7506dca81544d6e756ad
venkatesh nadar <venkatesh.nadar@neosofttech.com>
2020-09-14 22:21:46 +0530

    Refactored URL to implement dry

--- ajax_crud/crud/urls.py
+++ ajax_crud/crud/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.CrudView.as_view(), name = "index"),
-    path('user/create/', views.CreateCrudUser.as_view(), name = "create"),
-    path('user/update/', views.UpdateCrudUser.as_view(), name = "update"),
-    path('user/delete/', views.DeleteCrudUser.as_view(), name = "delete"),
+    path('user/', include([
+        path('create/', views.CreateCrudUser.as_view(), name = "create"),
+        path('update/', views.UpdateCrudUser.as_view(), name = "update"),
+        path('delete/', views.DeleteCrudUser.as_view(), name = "delete"),
+    ]))
 ]
